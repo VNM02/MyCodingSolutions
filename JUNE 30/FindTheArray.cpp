95dacb74f2c491386edb5edb8ac352c2b73262e6
Vaibhav Mahajan <92353375+VNM02@users.noreply.github.com>
2022-06-30 20:59:05 +0530

    JUNE 30 Commit

---
+++ JUNE 30/FindTheArray.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+typedef long long ll ;
+using namespace std ;
+int main(){
+int t;
+cin>>t;
+for(int j=0;j<t;j++)
+{
+	int n;
+	cin>>n;
+	int p=ceil(sqrt(n));
+	cout<<p<<"\n";
+}
+return 0;
+}
