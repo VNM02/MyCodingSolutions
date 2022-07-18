b5ee5d15244dc6a903e0d27939e99fb677f77e01
Vaibhav Mahajan <92353375+VNM02@users.noreply.github.com>
2022-07-18 20:28:33 +0530

    JULY 18 COMMIT

---
+++ JULY 18/ConstructString.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+typedef long long ll ;
+using namespace std ;
+int main(){
+int t;
+cin>>t;
+for(int j=0;j<t;j++)
+{
+	int n, a, b;
+		cin >> n >> a >> b;
+		for (int i = 0; i < n; ++i) {
+			cout << char('a' + i % b);
+		}
+		cout << endl;
+	
+}
+return 0;
+}
