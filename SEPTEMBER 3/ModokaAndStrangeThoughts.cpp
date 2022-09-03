f95d5c517b053476d1de977a6178a1e86f36c12b
Vaibhav Mahajan <92353375+VNM02@users.noreply.github.com>
2022-09-03 23:43:10 +0530

    SEPTEMBER 3 COMMIT

---
+++ SEPTEMBER 3/ModokaAndStrangeThoughts.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+typedef long long ll ;
+using namespace std ;
+void solve()
+{
+	int n;
+	cin>>n;
+	int ans=2*((n/3)+(n/2))+n;
+	cout<<ans<<"\n";
+}
+int main(){
+int t;
+cin>>t;
+for(int i=0;i<t;i++)
+{
+	solve();
+}
+return 0;
+}
