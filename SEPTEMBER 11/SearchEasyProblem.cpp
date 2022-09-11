95ae7505da8688ba9ace8fa0c37689d38406ea62
Vaibhav Mahajan <92353375+VNM02@users.noreply.github.com>
2022-09-11 21:45:40 +0530

    SEPTEMBER 11 COMMIT

---
+++ SEPTEMBER 11/SearchEasyProblem.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int tc;
+cin>>tc;
+int k=0;
+vector<int>v(tc);
+for(int i=0;i<tc;i++){
+ cin>>v[i];
+ if(v[i]==1){ k=1;}
+}
+if(k==1){cout<<"HARD"<<endl;}
+else {cout<<"EASY"<<endl;}
+}
+
+   	  								   				 			 		 	
