afb440f7c291bd810b3e97ab6f2b726657fc57b6
Vaibhav Mahajan <92353375+VNM02@users.noreply.github.com>
2022-09-12 22:21:40 +0530

    SEPTEMBER 12 COMMIT

---
+++ SEPTEMBER 12/Presents.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin>> n ;
+    int arr[n] , arr2[n] ;
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        cin>> arr[i] ;
+        arr2[arr[i]] = i ;
+
+    }
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        cout<< arr2[i] << " " ;
+    }
+    return 0;
+}
