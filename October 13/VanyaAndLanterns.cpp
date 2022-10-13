a9274bafd7ba6385d7a1bfe304b06f171efa90ef
Vaibhav Mahajan <92353375+VNM02@users.noreply.github.com>
2022-10-13 23:22:12 +0530

    OCTOBER 13 COMMIT

---
+++ October 13/VanyaAndLanterns.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+typedef long long ll ;
+using namespace std ;
+int main(){
+int n,l;
+cin>>n>>l;
+vector<double> vec;
+double d=0;
+for(int i=0;i<n;i++)
+{
+	double num;
+	cin>>num;
+	vec.push(num);
+}
+
+return 0;
+}
